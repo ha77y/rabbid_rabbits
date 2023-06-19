0c05e257b29b4d6ce04dfc3469d69ac040e424c0
ha77y <114589921+ha77y@users.noreply.github.com>
2023-06-19 16:35:46 +0100

    it kinda works better?

--- Assets/PlayerMoveScript.cs
+++ Assets/PlayerMoveScript.cs
@@ -16,13 +16,15 @@ public class PlayerMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerT.localPosition =
-            new Vector3(playerT.position.x  + (Input.GetAxis("Horizontal")*Time.deltaTime), 
-            playerT.position.y + Input.GetAxis("Vertical") * Time.deltaTime * Mathf.Sin(playerT.rotation.x * floatToRad), 
-            playerT.position.z + Input.GetAxis("Vertical") * Time.deltaTime * Mathf.Cos(playerT.rotation.x *floatToRad) );
+        playerT.localPosition = new Vector3(
+            playerT.position.x  + (Input.GetAxis("Horizontal")*Time.deltaTime),                                             //x
+            playerT.position.y + Input.GetAxis("Vertical") * Time.deltaTime * Mathf.Sin((playerT.rotation.x / floatToRad)*2 ),   //y
+            playerT.position.z + Input.GetAxis("Vertical") * Time.deltaTime * Mathf.Cos((playerT.rotation.x / floatToRad)*2 ));  //z
 
         playerT.rotation =Quaternion.Euler(playerT.rotation.eulerAngles.x + Input.GetAxis("Mouse Y")*-1, playerT.rotation.eulerAngles.y + Input.GetAxis("Mouse X"), playerT.rotation.eulerAngles.z);
 
-        
+        print(playerT.rotation.x * floatToRad);
+
+        //Mathf.Abs
     }
 }
