91fdc531b41fe8c6dd901930f70ea105f5f1a4cc
ha77y <114589921+ha77y@users.noreply.github.com>
2023-06-19 15:51:40 +0100

    movement update 1

--- Assets/PlayerMoveScript.cs
+++ Assets/PlayerMoveScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMoveScript : MonoBehaviour
 {
     public Transform playerT;
+    private const float floatToRad = Mathf.PI / 180;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +16,13 @@ public class PlayerMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerT.position = new Vector3(playerT.position.x + Input.GetAxis("Horizontal"), playerT.position.y, playerT.position.z + Input.GetAxis("Vertical"));
+        playerT.localPosition =
+            new Vector3(playerT.position.x  + (Input.GetAxis("Horizontal")*Time.deltaTime), 
+            playerT.position.y + Input.GetAxis("Vertical") * Time.deltaTime * Mathf.Sin(playerT.rotation.x * floatToRad), 
+            playerT.position.z + Input.GetAxis("Vertical") * Time.deltaTime * Mathf.Cos(playerT.rotation.x *floatToRad) );
 
+        playerT.rotation =Quaternion.Euler(playerT.rotation.eulerAngles.x + Input.GetAxis("Mouse Y")*-1, playerT.rotation.eulerAngles.y + Input.GetAxis("Mouse X"), playerT.rotation.eulerAngles.z);
+
+        
     }
 }
