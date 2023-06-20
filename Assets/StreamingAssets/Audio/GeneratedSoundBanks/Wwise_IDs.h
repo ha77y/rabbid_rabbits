a9d684bd3af84c15c5c859d80dc56a3020c28d1c
ha77y <114589921+ha77y@users.noreply.github.com>
2023-06-20 09:50:15 +0100

    wwise and fix and downgrade
    3.11f1

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,94 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID CANDLE_EXTINGUISHED = 920148974U;
+        static const AkUniqueID ENDMUSIC = 1681803181U;
+        static const AkUniqueID PLAY_BREATHINGSOUNDS = 3171898892U;
+        static const AkUniqueID PLAY_PAGES_SOUNDS = 1558645989U;
+        static const AkUniqueID PLAY_WOOSH = 1725216094U;
+        static const AkUniqueID STARTCLOCK = 3764788319U;
+        static const AkUniqueID STARTMAIN = 3356728648U;
+        static const AkUniqueID STARTMENUMUSIC = 2440264179U;
+        static const AkUniqueID STARTRAIN = 2407527377U;
+        static const AkUniqueID STOP_BREATHINGSOUNDS = 1291076182U;
+        static const AkUniqueID STOP_PAGES_SOUNDS = 1149349919U;
+        static const AkUniqueID STOPRAIN = 856242967U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace PLAYERLIFE
+        {
+            static const AkUniqueID GROUP = 444815956U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ALIVE = 655265632U;
+                static const AkUniqueID DEAD = 2044049779U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace PLAYERLIFE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace INSANITY
+        {
+            static const AkUniqueID GROUP = 34996162U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID LEVEL1 = 2678230382U;
+                static const AkUniqueID LEVEL2 = 2678230381U;
+                static const AkUniqueID LEVEL3 = 2678230380U;
+                static const AkUniqueID LEVEL4 = 2678230379U;
+            } // namespace SWITCH
+        } // namespace INSANITY
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID JUMPSCAREPOSITION = 1505334234U;
+        static const AkUniqueID MASTERVOLUME = 2918011349U;
+        static const AkUniqueID MUSICVOLUME = 2346531308U;
+        static const AkUniqueID SANITY = 2938993575U;
+        static const AkUniqueID SFXVOLUME = 988953028U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID ENVIRONMENT = 1229948536U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
