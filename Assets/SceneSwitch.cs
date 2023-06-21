0c45a23c00dfe8978a25ec659bc3aa33e303cfa0
ha77y <114589921+ha77y@users.noreply.github.com>
2023-06-21 12:28:30 +0100

    tmp + hdri sky
    yum

---
+++ Assets/SceneSwitch.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    
+    public void Quit() 
+    {
+        Application.Quit();
+    }
+
+    public void SwitchScene(int value)
+    {
+        SceneManager.LoadScene(value);
+    }
+}
