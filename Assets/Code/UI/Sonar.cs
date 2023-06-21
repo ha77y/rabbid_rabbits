0cd89acbeb2c83c6e78e056e6c820aeac2b974cc
Georgonzola <94077155+Georgonzola@users.noreply.github.com>
2023-06-21 11:26:41 +0100

    Sonar

--- Assets/Code/UI/Sonar.cs
+++ Assets/Code/UI/Sonar.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Sonar : MonoBehaviour
 {
 
     public Character player;
 
+    public float rotation = 0;
 
+    public Image sonarBase;
 
+    public Image sonarSweep;
     // Start is called before the first frame update
 
     void Start()
@@ -19,6 +24,12 @@ public class Sonar : MonoBehaviour
 
     void Update()
     {
-        
+        rotation = player.transform.eulerAngles.y;
+        sonarBase.transform.rotation = Quaternion.Euler(0, 0, -rotation);
+
+        Debug.Log(rotation);
+
+
+        sonarSweep.transform.Rotate(0, 0, 1);
     }
 }
--- Assets/Code/UI/Sonar_Dot.cs
+++ Assets/Code/UI/Sonar_Dot.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Sonar_Dot : MonoBehaviour
 {
@@ -14,5 +15,10 @@ public class Sonar_Dot : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void resetLifespan()
+    {
+        lifespan = 3f;
     }
 }
