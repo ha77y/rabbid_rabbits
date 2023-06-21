912e1ee408de15c2e1ea20f9a8c5362b9e3386fa
Sono00 <sonofstoney00@gmail.com>
2023-06-21 15:48:37 +0100

    flare7

--- Assets/Code/Flareshot.cs
+++ Assets/Code/Flareshot.cs
@@ -26,6 +26,7 @@ public class Flareshot : MonoBehaviour
             if(player.numFlare >= 1)
             {
                 rotation.eulerAngles = new Vector3(player.lookDirection.y, player.lookDirection.x, player.lookDirection.z);
+                Destroy(currentflare);
                 currentflare = Instantiate(Flare,player.transform.position+player.transform.forward * 1,rotation);
                 rb = currentflare.GetComponent<Rigidbody>();
                 rb.AddForce(transform.forward * Thrust,ForceMode.Impulse);
